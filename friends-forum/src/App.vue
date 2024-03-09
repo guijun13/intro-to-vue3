269f7b1c0376460be04adae51d992de4d02ccc3d
guijun13 <guilhermejun13.grillo@gmail.com>
2024-03-09 16:16:05 -0300

    create vite project for tv-show forum

---
+++ friends-forum/src/App.vue
@@ -0,0 +1,95 @@
+<script>
+  export default {
+    data: () => ({
+          friendsCharactersList: [
+            {
+              name: "Ross Geller",
+              gender: ["Male"],
+            },
+            {
+              name: "Monica Geller",
+              gender: ["Female"],
+            },
+            {
+              name: "Chandler Bing",
+              gender: ["Male"],
+            },
+            {
+              name: "Joey Tribbiani",
+              gender: ["Male"],
+            },
+            {
+              name: "Rachel Green",
+              gender: ["Female"],
+            },
+            {
+              name: "Phoebe Buffay",
+              gender: ["Female"],
+            },
+            // {
+            //   name: "Janice"
+            // }
+          ],
+          favoriteCharactersList: [],
+          newCharacter: {
+            name: "",
+            gender: [""]
+          },
+        }),
+        computed: {
+          genderStatistics(){
+            const genders = ["Male", "Female"];
+            const statistics = {
+              Male: 0,
+              Female: 0
+            }
+
+            this.friendsCharactersList.forEach(character => {
+              genders.forEach(gender => {
+                if(character.gender.indexOf(gender) > -1){
+                  statistics[gender] += 1
+                }
+              })
+            })
+
+            return statistics;
+          }
+        },
+        methods: {
+          favorite(character) {
+            this.favoriteCharactersList.push(character);
+          },
+          addNewCharacter(){
+            this.friendsCharactersList.push(this.newCharacter);
+            this.newCharacter = { name: "" };
+          }
+        }
+  }
+
+</script>
+
+<template>
+  <ul>
+        <li v-for="character in friendsCharactersList" v-bind:key="character.name">
+          {{ character.name }}
+          <button v-on:click="favorite(character)">Favorite</button>
+        </li>
+      </ul>
+      <input type="text" v-model="newCharacter.name" @keyup.enter="addNewCharacter"/>
+      <button @click="addNewCharacter">Add Character</button>
+    <!-- </hr> -->
+    <h2>Favorite Characters</h2>
+    <p v-if="favoriteCharactersList.length === 0">There are no favorite characters yet</p>
+    <ul v-else>
+      <li v-for="character in favoriteCharactersList" v-bind:key="character.name">
+        {{ character.name }}
+      </li>
+    </ul>
+    <h2>Statistics</h2>
+    <ul>
+      <li v-for="(stat, type) in genderStatistics" v-bind:key="type">
+        {{ type }}: {{ stat }}
+      </li>
+    </ul>
+</template>
+
