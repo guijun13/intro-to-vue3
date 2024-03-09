e6e9fc347535163be6e394c179f481480b40f565
guijun13 <guilhermejun13.grillo@gmail.com>
2024-03-09 16:54:27 -0300

    creater baseCounter component

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,10 +1,12 @@
 <script>
-  export default {
+import BaseCounter from './components/BaseCounter.vue';
+
+export default {
+    components: {
+      BaseCounter,
+    },
     data() {
           return {
-            count: 0,
-            counterTitle: "Counter standard",
-            incrementAmount: 10,
             message: "Hello, Vue 3",
             listOfNumbers: [1, 2, 3, 4, 5],
             listOfObjects: [
@@ -15,26 +17,6 @@
             ],
           };
         },
-        computed: {
-          displayTitle() {
-            if(this.count > 20){
-              return "Counter is greater than 20";
-            } else {
-              return "Counter is less than or equal to 20";
-            }
-          },
-          optimizedAmount() {
-            return this.displayTitle.length * this.incrementAmount;
-          },
-        },
-        methods: {
-          // changeIncrementAmount(event){
-          //   this.incrementCount += Number(event.target.value);
-          // },
-          incrementCount() {
-            this.count += this.incrementAmount;
-          },
-        },
         watch: {
           count(newValue){
             console.log('watch: ', newValue);
@@ -44,16 +26,7 @@
 </script>
 
 <template>
-      <h1>Counter</h1>
-      <p>{{ count }}</p>
-      <button v-on:click="incrementCount">Increment</button>
-      <p>{{ displayTitle }}</p>
-      <p>{{ optimizedAmount }}</p>
-      <div>
-        <label for="incrementAmount">Increment by:</label>
-        <!-- <input type="number" v-bind:value="incrementAmount" v-on:input="changeIncrementAmount"> -->
-        <input type="number" v-model="incrementAmount" >
-      </div>
+      <BaseCounter />
 
       <hr />
 
@@ -97,4 +70,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style>./components/BaseCounter.vue
